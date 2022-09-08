4da1b778d3d48b0e06baab956b51c5a01cb202f9
Kazi Mamun <kazimamun716@gmail.com>
2022-09-08 22:27:53 +0600

    some error set

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { register } from '../../store/actions/authActions';
@@ -15,7 +15,16 @@ const Register = (props) => {
 
     //connect redux help of react-redux
     const auth = useSelector(state=>state);
-    console.log(auth)
+    useEffect(()=>{
+        console.log(auth, user)
+        if(auth.auth.error?.name || auth.auth.error?.email || auth.auth.error?.password || auth.auth.error?.confirmPassword){
+            console.log('error',auth.auth.error)
+            setUser({
+                ...user,
+                error:auth.auth.error
+            })
+        }
+    },[auth])
     const handleChange=e=>{
         setUser({
             ...user,
@@ -42,11 +51,12 @@ const Register = (props) => {
                             type="text" 
                             placeholder='Enter Your Name'
                             onChange={handleChange}
-                            className={user.error.name? 'form-control invalid-feedback':'form-control'}
+                            className={user.error.name? 'form-control is-invalid':'form-control'}
                             name='name'
                             id='name'
                             value={user.name||''}
                         />
+                        <p className='invalid-feedback'>{user.error.name}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor='email'>Email:</label>
@@ -54,11 +64,12 @@ const Register = (props) => {
                             type="email" 
                             placeholder='Enter Your Email'
                             onChange={handleChange}
-                            className={user.error.email? 'form-control invalid-feedback':'form-control'}
+                            className={user.error.email? 'form-control is-invalid':'form-control'}
                             name='email'
                             id='email'
                             value={user.email||''}
                         />
+                        <p className='invalid-feedback'>{user.error.email}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor='password'>Password:</label>
@@ -66,11 +77,13 @@ const Register = (props) => {
                             type="password" 
                             placeholder='Enter Your Password'
                             onChange={handleChange}
-                            className={user.error.password? 'form-control invalid-feedback':'form-control'}
+                            className={user.error.password? 'form-control is-invalid':'form-control'}
                             name='password'
                             id='password'
                             value={user.password||''}
                         />
+                        
+                        <p className='invalid-feedback'>{user.error.password}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor='confirmPassword'>confirmPassword:</label>
@@ -78,11 +91,13 @@ const Register = (props) => {
                             type="password" 
                             placeholder='Enter Your Password Again'
                             onChange={handleChange}
-                            className={user.error.confirmPassword? 'form-control invalid-feedback':'form-control'}
+                            className={user.error.confirmPassword? 'form-control is-invalid':'form-control'}
                             name='confirmPassword'
                             id='confirmPassword'
                             value={user.confirmPassword||''}
                         />
+                        
+                        <p className='invalid-feedback'>{user.error.confirmPassword}</p>
                     </div>
                     <input type="submit" className="btn btn-primary my-2" value='Register' />
                 </form>
